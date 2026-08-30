3442d16b3e062f624ec0579d30b6a4cdf0b28407
mohammed hussein‬‎ <64332258+iegy@users.noreply.github.com>
2026-08-31 00:33:23 +0300

    Bump native app to v2.1.0

--- tajweed-fatiha-v1.0.2/app/build.gradle.kts
+++ tajweed-fatiha-v1.0.2/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.iegy.tajweed.fatiha"
         minSdk = 23
         targetSdk = 35
-        versionCode = 5
-        versionName = "2.0.1"
+        versionCode = 6
+        versionName = "2.1.0"
     }
 
     buildTypes {
