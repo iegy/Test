937d8a671cd0eb4c666d53acd55382a0c85184f5
mohammed hussein‬‎ <64332258+iegy@users.noreply.github.com>
2026-08-30 23:33:41 +0300

    Bump native hotfix to 2.0.1

--- tajweed-fatiha-v1.0.2/app/build.gradle.kts
+++ tajweed-fatiha-v1.0.2/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.iegy.tajweed.fatiha"
         minSdk = 23
         targetSdk = 35
-        versionCode = 4
-        versionName = "2.0.0"
+        versionCode = 5
+        versionName = "2.0.1"
     }
 
     buildTypes {
